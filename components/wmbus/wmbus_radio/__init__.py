f6ae9324203271d53c34529c25f3303cb4890f08
blakinio <75369544+blakinio@users.noreply.github.com>
2025-08-21 09:27:10 +0200

    Refactor wmbus radio pin handling

--- components/wmbus/wmbus_radio/__init__.py
+++ components/wmbus/wmbus_radio/__init__.py
@@ -3,7 +3,6 @@ import esphome.codegen as cg
 from esphome.components import spi
 from esphome import pins
 from esphome.const import (
-    CONF_CS_PIN,
     CONF_RESET_PIN,
     CONF_FREQUENCY,
     CONF_ID,
@@ -13,7 +12,8 @@ DEPENDENCIES = ["spi"]
 MULTI_CONF = True
 
 wmbus_radio_ns = cg.esphome_ns.namespace("wmbus_radio")
-WMBusRadio = wmbus_radio_ns.class_("WMBusRadio", cg.Component, spi.SPIDevice)
+RadioComponent = wmbus_radio_ns.class_("Radio", cg.Component, spi.SPIDevice)
+Radio = RadioComponent
 
 # Configuration keys
 CONF_RADIO_TYPE = "radio_type"
@@ -50,9 +50,8 @@ def validate_radio_config(config):
 
 CONFIG_SCHEMA = cv.All(
     cv.Schema({
-        cv.GenerateID(): cv.declare_id(WMBusRadio),
+        cv.GenerateID(): cv.declare_id(RadioComponent),
         cv.Required(CONF_RADIO_TYPE): cv.one_of(*RADIO_TYPES, upper=True),
-        cv.Required(CONF_CS_PIN): pins.gpio_output_pin_schema,
         cv.Optional(CONF_RESET_PIN): pins.gpio_output_pin_schema,
         cv.Optional(CONF_FREQUENCY, default=868.95): cv.float_range(min=300.0, max=928.0),
         
@@ -71,13 +70,9 @@ async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
     await spi.register_spi_device(var, config)
-    
+
     # Set radio type
     cg.add(var.set_radio_type(config[CONF_RADIO_TYPE]))
-    
-    # Set CS pin
-    cs_pin = await cg.gpio_pin_expression(config[CONF_CS_PIN])
-    cg.add(var.set_cs_pin(cs_pin))
     
     # Set reset pin if provided
     if CONF_RESET_PIN in config:
@@ -91,10 +86,10 @@ async def to_code(config):
     if config[CONF_RADIO_TYPE] == "CC1101":
         if CONF_GDO0_PIN in config:
             gdo0_pin = await cg.gpio_pin_expression(config[CONF_GDO0_PIN])
-            cg.add(var.set_gdo0_pin(gdo0_pin))
+            cg.add(var.set_data_pin(gdo0_pin))
         if CONF_GDO2_PIN in config:
             gdo2_pin = await cg.gpio_pin_expression(config[CONF_GDO2_PIN])
-            cg.add(var.set_gdo2_pin(gdo2_pin))
+            cg.add(var.set_sync_pin(gdo2_pin))
             
     elif config[CONF_RADIO_TYPE] == "SX1276":
         if CONF_IRQ_PIN in config:
